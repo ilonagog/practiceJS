518fe43732faac8d25711007fac941057f14e916
ilona <gogiashvili.ilona@gmail.com>
2025-11-17 18:40:43 -0500

    30 Days JavaScript

--- js30Days.js
+++ js30Days.js
@@ -14,3 +14,28 @@
 //     return n++;
 //   };
 // }
+
+// 3. 2704. To Be Or Not To Be
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+// function expect (val){
+//     return {
+//         toBe: function (otherVal){
+//             if (val === otherVal){
+//                 return true;
+//             }else{
+//                 throw new Error("Not Equal");
+//             }
+//         },
+//         notToBe: function(otherVal){
+//             if (val !== otherVal){
+//                 return true;
+//             }else{
+//                 throw new Error("Equal");
+//             }
+//         }
+//     };
+// }

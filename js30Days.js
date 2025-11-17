ee28966c1b6deca627ee5e6ba3084f61a8a28f60
ilona <gogiashvili.ilona@gmail.com>
2025-11-17 16:50:26 -0500

    30 Days JavaScript

--- js30Days.js
+++ js30Days.js
@@ -6,3 +6,11 @@
 // }
 // const result = createHelloWorld();
 // console.log(result());
+
+// 2 2620. Counter
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+// function createCounter(n) {
+//   return function () {
+//     return n++;
+//   };
+// }

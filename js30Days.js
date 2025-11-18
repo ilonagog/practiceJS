66697bd0a9f5689e4eee5a674169d4c10f616907
ilona <gogiashvili.ilona@gmail.com>
2025-11-17 19:08:09 -0500

    30 Days JavaScript

--- js30Days.js
+++ js30Days.js
@@ -39,3 +39,28 @@
 //         }
 //     };
 // }
+
+// 4 2665. Counter II
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+// function createCounter(init){
+//     let value = init
+//     return {
+//         increment: function (){
+//             return ++value;
+//         },
+//         decrement: function (){
+//             return --value;
+//         },
+//         reset: function (){
+//             value = init;
+//             return value;
+//         },
+//     };
+// };

cd845ad50770064cfbc4631887a578bb1fddb6d5
ilona <gogiashvili.ilona@gmail.com>
2025-11-23 03:35:21 -0500

    30 Days JavaScript

--- js30Days.js
+++ js30Days.js
@@ -64,3 +64,18 @@
 //         },
 //     };
 // };
+// 5.2635. Apply Transform Over Each Element in Array
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+// function map(arr, fn) {
+//     const result = [];
+//     for(let i=0; i < arr.length; i++){
+//         result.push(fn(arr[i], i));
+//     }
+//     return result;
+// };
